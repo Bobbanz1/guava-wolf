11ff5f3676e716d4c7063665b2bb85983532ec7a
Linux <bobbanz14@gmail.com>
2024-08-28 12:30:14 +0200

    feat(dashboard/): adds stuff

--- src/gw/dashboard/main.py
+++ src/gw/dashboard/main.py
@@ -1,6 +1,7 @@
 from gw.common import database
 from pymongo import MongoClient
 import json
+from datetime import datetime, timedelta
 from flask import Flask, Response, render_template, stream_with_context, jsonify
 
 # Starts up a new Flask Instance
@@ -23,7 +24,12 @@ def index():
     results = []
     for item in data:
         item['_id'] = str(item['_id'])
-        results.append(item)
+        acquired_time = datetime.strptime(item['timestamp'], '%Y-%m-%d %H:%M')
+        time_now = datetime.strptime(datetime.now().strftime("%Y-%m-%d %H:%M"), '%Y-%m-%d %H:%M')
+        if acquired_time < time_now - timedelta(minutes=8):
+            continue
+        else:
+            results.append(item)
     # Sort the list by the timestamp to ensure we are putting stuff on the graph in the correct direction
     results.sort(key=SortByDate)
     return render_template('index.html', things = results)
@@ -35,7 +41,12 @@ def api_datapoint():
     results = []
     for item in data:
         item['_id'] = str(item['_id'])
-        results.append(item)
+        acquired_time = datetime.strptime(item['timestamp'], '%Y-%m-%d %H:%M')
+        time_now = datetime.strptime(datetime.now().strftime("%Y-%m-%d %H:%M"), '%Y-%m-%d %H:%M')
+        if acquired_time < time_now - timedelta(minutes=8):
+            continue
+        else:
+            results.append(item)
     # Sort the list by the timestamp to ensure we are putting stuff on the graph in the correct direction
     results.sort(key=SortByDate)
     return jsonify({'results':results})
